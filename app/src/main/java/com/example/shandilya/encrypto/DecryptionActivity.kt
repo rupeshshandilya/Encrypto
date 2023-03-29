b5f2f93608cce3c8b77d073b5ec762cddf6bb151
RupeshShandilya2003 <rupeshkshandilya@gmail.com>
2023-03-29 22:57:42 +0530

    implemented open gallery functionality

--- app/src/main/java/com/example/shandilya/encrypto/DecryptionActivity.kt
+++ app/src/main/java/com/example/shandilya/encrypto/DecryptionActivity.kt
@@ -8,21 +8,26 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import java.io.ByteArrayOutputStream
 
 class DecryptionActivity : AppCompatActivity() {
     private var key: EditText? = null
     private var decryptImage: TextView? = null
     private var decodeText: TextView?= null
     private var decryptButton: Button? = null
+    private var bitMap: Bitmap? = null
 
     //For Dialog Box
     private var builder: AlertDialog.Builder? = null
@@ -51,8 +56,38 @@ class DecryptionActivity : AppCompatActivity() {
         }
 
         decryptImage!!.setOnClickListener {
+            //For Opening Gallery
+            try{
+                val intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                intent.type=("image/*")
+                val mediaType = arrayOf("image/jpg","image/png","image/jpeg")
+                intent.putExtra(Intent.EXTRA_MIME_TYPES, mediaType)
+                intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                startActivityForResult(intent,1)
 
+            }
+            catch (e: Exception){
+                Toast.makeText(this,"Failed to Open Gallery😔",Toast.LENGTH_SHORT).show()
+                e.printStackTrace()
+            }
         }
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK){
+            data?.data.let { uri ->
+                try{
+                   bitMap = MediaStore.Images.Media.getBitmap(this.contentResolver,uri)
+                   val outputStream = ByteArrayOutputStream()
+                    bitMap!!.compress(Bitmap.CompressFormat.PNG,100,outputStream)
+                    Toast.makeText(this,"Image added successfully😀",Toast.LENGTH_SHORT).show()
+                }
+                catch (e: Exception){
+                    Toast.makeText(this,"Failed to Open Gallery😔",Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 }
